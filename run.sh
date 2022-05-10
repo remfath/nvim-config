01fec55d98e4f3a4cb19324f036c2a81751b90f3
remfath <remfath@gmail.com>
2022-05-10 14:38:23 +0800

    remove .github

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
 cd local
 find . -type d -name ".git" | xargs rm -rf
+find . -type d -name ".github" | xargs rm -rf
 
 cd ../config
 find . -type d -name ".git" | xargs rm -rf
+find . -type d -name ".github" | xargs rm -rf
 
 cd ../cache
 find . -type d -name ".git" | xargs rm -rf
+find . -type d -name ".github" | xargs rm -rf
